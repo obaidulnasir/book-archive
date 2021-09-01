ce9835388ad8b50481177c29b135f66a63cd86c8
Obaidul Nasir <83164409+obaidulnasir@users.noreply.github.com>
2021-09-02 02:37:24 +0600

    trying fetch

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log("js file connected")
+// const inputField = document.getElementById("input-field");
+// const inputValue = inputField.value;
+// const searchButton = document.getElementById("search-button");
+
+const searchItem = () => {
+  const inputField = document.getElementById("input-field");
+  const inputValue = inputField.value;
+  const url = `http://openlibrary.org/search.json?q=${inputValue}`;
+  fetch(url)
+  .then(res => res.json())
+  .then(data => getBookInfo(data.docs))
+};
+
+
+const getBookInfo = (bookData) => {
+    for (const book of bookData){
+        console.log(book.title);
+    }
+}
