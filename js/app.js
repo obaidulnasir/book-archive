d6514b65b4389ee5677e3e9ebf0b04350a04a58a
Obaidul Nasir <83164409+obaidulnasir@users.noreply.github.com>
2021-09-02 15:25:01 +0600

    getting single book data by useing for eachb

--- js/app.js
+++ js/app.js
@@ -1,19 +1,32 @@
-// const inputField = document.getElementById("input-field");
-// const inputValue = inputField.value;
-// const searchButton = document.getElementById("search-button");
+const inputField = document.getElementById("input-field");
+const searchButton = document.getElementById("search-button");
 
-const searchItem = () => {
-  const inputField = document.getElementById("input-field");
+searchButton.addEventListener("click" , function(){
   const inputValue = inputField.value;
-  const url = `http://openlibrary.org/search.json?q=${inputValue}`;
+  const url = `http://openlibrary.org/search.json?q=${inputValue}`
   fetch(url)
   .then(res => res.json())
-  .then(data => getBookInfo(data.docs))
-};
+  .then(data => bookInfo(data.docs))
+})
 
-
-const getBookInfo = (bookData) => {
-    for (const book of bookData){
-        console.log(book.title);
-    }
+const bookInfo = (bookData) => {
+  // console.log(bookData)
+  bookData.forEach(item => {
+    
+  });
 }
+
+// const searchItem = () => {
+//   const inputField = document.getElementById("input-field");
+//   const inputValue = inputField.value;
+//   const url = `http://openlibrary.org/search.json?q=${inputValue}`;
+//   fetch(url)
+//     .then((res) => res.json())
+//     .then((data) => getBookInfo(data.docs));
+// };
+
+// const getBookInfo = (bookData) => {
+//   for (const book of bookData) {
+//     console.log(book.title);
+//   }
+// };
